db6b0cfe1a3e188cc63519e03f68f6a39d1a80cc
彥霖 陳 <yan0988777698@gmail.com>
2024-05-11 17:56:22 +0800

    First version

---
+++ projFitConnect/Controllers/TrainerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projFitConnect.Controllers
+{
+    public class TrainerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
