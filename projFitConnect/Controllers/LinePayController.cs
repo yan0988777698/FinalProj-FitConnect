cadc2099ab1efa452dd06d59c9a435b3f9f55d0d
彥霖 陳 <yan0988777698@gmail.com>
2024-06-09 12:49:55 +0800

    #LinePay付款

---
+++ projFitConnect/Controllers/LinePayController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projFitConnect.Controllers
+{
+    public class LinePayController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+        public IActionResult Confirm()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ projFitConnect/Controllers/SchduleController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projFitConnect.Controllers
+{
+    public class SchduleController : Controller
+    {
+        public IActionResult Update(int? id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Update()
+        {
+            return RedirectToAction("Profile", "Coach");
+        }
+    }
+}
